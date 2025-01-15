87aea7fe295ec039d22d8adfb2660ef4b0d744ff
Angel Morales <amorales10@huskers.unl.edu>
2025-01-15 16:07:33 -0600

    Income Graph Implementation

--- Front End/src/Home.js
+++ Front End/src/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, use } from "react";
 import { variables } from "./Variables";
-import { Bar } from "react-chartjs-2";
+import { Bar, Doughnut } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   BarElement,
@@ -8,9 +8,19 @@ import {
   LinearScale,
   Tooltip,
   Legend,
+  DoughnutController,
+  ArcElement,
 } from "chart.js";
 
-ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+  DoughnutController,
+  ArcElement
+);
 
 export class Home extends Component {
   constructor(props) {
@@ -38,6 +48,7 @@ export class Home extends Component {
 
   render() {
     const { monthlySpending } = this.state;
+    const TotalIncome = "$25,000";
 
     const labels = monthlySpending.map((entry) => entry.MonthYear); // X-axis labels
     const spendingData = monthlySpending.map((entry) => entry.TotalSpending); // Y-axis values
@@ -84,10 +95,90 @@ export class Home extends Component {
       },
     };
 
+    const doughnutData = {
+      labels: ["Salary", "Investements", "Savings"],
+      datasets: [
+        {
+          label: "Income",
+          data: [300, 90, 100],
+          backgroundColor: [
+            "rgb(255,99,132)",
+            "rgb(54,162,235)",
+            "rgb(57,255,20)",
+          ],
+          hoversetOffset: 4,
+        },
+      ],
+    };
+
+    const doughnutConfig = {
+      responsive: true,
+      type: "doughnut",
+      data: doughnutData,
+      cutout: "95%",
+      plugins: {
+        legend: {
+          position: "bottom",
+          labels: {
+            usePointStyle: true,
+            padding: 10,
+          },
+        },
+      },
+    };
+
+    const centerTextPlugin = {
+      id: "centerText",
+      beforeDatasetsDraw: (chart, args, plugins) => {
+        const { width, ctx } = chart;
+        const { top, bottom } = chart.chartArea;
+
+        const xCenter = width / 2;
+        const yCenter = top + bottom / 2;
+
+        const largeFontSize = Math.round(width / 8);
+        const smallFontSize = Math.round(width / 20);
+
+        ctx.save();
+        ctx.font = `${largeFontSize}px Arial`;
+        ctx.textAlign = "center";
+        ctx.fillStyle = "black";
+        ctx.fillText(TotalIncome, xCenter, yCenter - 10);
+
+        ctx.font = `${smallFontSize}px Arial`;
+        ctx.fillStyle = "grey";
+        ctx.fillText("Total Income", xCenter, yCenter + 25);
+        ctx.restore();
+      },
+    };
+
     return (
       <div>
         <h3>This is the Home page!</h3>
-        <div style={{ width: "80%", margin: "0 auto" }}>
+
+        <div
+          style={{ display: "flex", width: "100%", justifyContent: "center" }}
+        >
+          <div>
+            {" "}
+            <Doughnut
+              data={doughnutData}
+              options={doughnutConfig}
+              plugins={[centerTextPlugin]}
+            />
+          </div>
+          <div>
+            {" "}
+            <Doughnut
+              data={doughnutData}
+              options={doughnutConfig}
+              plugins={[centerTextPlugin]}
+            />
+          </div>
+        </div>
+        <div
+          style={{ display: "flex", width: "80%", justifyContent: "center" }}
+        >
           <Bar data={data} options={options} />
         </div>
       </div>
